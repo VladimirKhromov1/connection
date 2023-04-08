1f748ecf22b92bab5a52ea7ac7f2a4f9a39215bb
v.khromov@datarockets.com <vladimir>
2023-04-09 01:01:44 +0300

    add like/dislike for slides

--- app/frontend/components/Browse.svelte
+++ app/frontend/components/Browse.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { fade, fly } from 'svelte/transition';
     import Fa from 'svelte-fa'
     import { faTimesCircle,faHeart } from '@fortawesome/free-solid-svg-icons'
     import { onMount } from 'svelte';
@@ -7,15 +8,28 @@
     export let accounts: Account
 
     $: index = 0
+    $: clickApprove = false
+    $: clickDecline = false
+    let like = false
+    let dislike = false
     $: select = accounts[index]
+    $: prev = accounts[index - 1]
     $: url = accounts[index].photo
    const approve = () => {
+     like = true
+     clickApprove = true
      index = index + 1
+     setTimeout(() => (like = false), 200)
+     setTimeout(() => (clickApprove = false), 700)
      console.log(index)
    }
 
    const decline = () => {
+     dislike = true
+     clickDecline = true
      index = index + 1
+     setTimeout(() => (dislike = false), 200)
+     setTimeout(() => (clickDecline = false), 700)
      console.log(index)
    }
 
@@ -24,7 +38,9 @@
 <div class="gen-container">
 <div class="slides">
     {#each accounts as account }
-            <div class="flip-card" class:showing={account == select}>
+        <span class="like" class:showing={like} transition:fade>Like!</span>
+        <span class="dislike" class:showing={dislike} transition:fade>Dislike!</span>
+            <div class="flip-card" class:showing={account == select} class:rotate-left={clickDecline && account == prev}  class:rotate-right={clickApprove && account == prev} out:fade>
                 <div class="flip-card-inner">
                     <div class="flip-card-front">
                         <img src={url} alt="Avatar" style="width:600px;height:600px;">
@@ -40,24 +56,76 @@
 </div>
 
 <div class="slide-controls">
-  <span id="decline" on:click={decline}><Fa icon={faTimesCircle} size="4x" /></span>
-  <span id="approve" on:click={approve}><Fa icon={faHeart} size="4x"/></span>
+  <span class="decline" on:click={decline}><Fa icon={faTimesCircle} size="4x" /></span>
+  <span class="approve" on:click={approve}><Fa icon={faHeart} size="4x"/></span>
 </div>
 
 </div>
 
 <style>
+    .like {
+        opacity: 0;
+        z-index: 0;
+        border-radius: 5px;
+        padding: 5px 10px;
+        border: 2px solid green;
+        color: green;
+        text-transform: uppercase;
+        font-size: 15px;
+        position: absolute;
+        top: 50px;
+        right: 38%;
+        text-shadow: none;
+    }
+    .dislike {
+        opacity: 0;
+        z-index: 1;
+        border-radius: 5px;
+        padding: 5px 10px;
+        border: 2px solid red;
+        color: red;
+        text-transform: uppercase;
+        font-size: 15px;
+        position: absolute;
+        top: 50px;
+        left: 35%;
+        text-shadow: none;
+    }
+    .rotate-right {
+        transform: rotate(30deg) scale(0.8);
+        transition: 1s;
+        margin-left: 400px;
+        cursor: e-resize;
+        opacity: 0;
+        z-index: 10;
+    }
+    .rotate-left {
+        transform: rotate(-30deg) scale(0.8);
+        transition: 1s;
+        opacity: 0;
+        margin-left: -400px;
+        cursor: w-resize;
+        z-index: 10;
+    }
     .slide-controls {
-        margin-top: 10rem;
+        margin-top: 40rem;
         display: flex;
         justify-content: space-between;
-        padding-left: 5%;
+        z-index: 2;
     }
-    #decline, #approve {
+
+    .decline {
         width: 150px;
+        color: #CDD6DD;
+    }
+
+    .approve {
+        width: 150px;
+        color: #FFACE4;
     }
 
     .gen-container {
+        position: relative;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -65,9 +133,11 @@
     }
 
     .flip-card {
+        left: 32%;
+        position: absolute;
+        box-sizing: border-box;
         border-radius: 5px;
         opacity: 0;
-        z-index: 1;
         background-color: transparent;
         width: 600px;
         height: 500px;
